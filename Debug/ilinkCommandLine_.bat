a6563c837756064b5845720abf520cab3af72e51
Leonardo Pereira <leonardo.pereira@intranet.agres>
2016-08-26 14:29:30 -0300

    fertig dude

--- Debug/ilinkCommandLine_.bat
+++
@@ -1,1 +0,0 @@
-cmd /c " --config "C:/Program Files (x86)/IAR Systems/Embedded Workbench 7.0/arm/config/linker/NXP/LPC1788.icf" --no_wrap_diagnostics --semihosting  && ielftool --checksum __vector_table+0x1c:4,sum32:2;__vector_table-__vector_table+0x1B --verbose  "
